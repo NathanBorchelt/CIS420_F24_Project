3511edb50371f90f2b517a9f916db114cebc3c3e
NathanBorchelt <nathan.d.borchelt@gmail.com>
2024-10-20 14:45:09 -0500

    Should be fixed with XML read in

--- src/pysterdesign_single/CPU.py
+++ src/pysterdesign_single/CPU.py
@@ -41,21 +41,21 @@ class CompUnit(object):
         self.flopsPerCycle = flopsPerCycle
 
         #print()
-        #try:
-        if isJSON:
-            for level, cacheSize in self.cache.items():
-                allData = self.model
-                print(cacheSize)
-                cacheData = cacheSize.split('b')
-                print(cacheData)
-                cacheData[1] = sizeBinaryLong[int(cacheData[1])]
-                self.cache[level] = " ".join(cacheData)
-        # except Exception as e:
-        #     print(e)
-        #     print("error with the data conversion of the cache for the following chip")
-        #     print(self.brand, self.subBrand, self.codeName, self.model, self.cpuConfigs, self.price, self.cores, self.cache, self.tdp, self.memorySpecs, self.clockSpeed, self.featureSize, self.flopsPerCycle)
-        #     e_exit()
-        # #print(self.cache)
+        try:
+            if isJSON:
+                for level, cacheSize in self.cache.items():
+                    allData = self.model
+                    print(cacheSize)
+                    cacheData = cacheSize.split('b')
+                    print(cacheData)
+                    cacheData[1] = sizeBinaryLong[int(cacheData[1])]
+                    self.cache[level] = " ".join(cacheData)
+        except Exception as e:
+            print(e)
+            print("error with the data conversion of the cache for the following chip")
+            print(self.brand, self.subBrand, self.codeName, self.model, self.cpuConfigs, self.price, self.cores, self.cache, self.tdp, self.memorySpecs, self.clockSpeed, self.featureSize, self.flopsPerCycle)
+            e_exit()
+        #print(self.cache)
 
     def __str__(self) ->  str:
         return "{bnd} {sbnd} {cdnme} {mdl}".format(bnd = self.brand, sbnd = self.subBrand, cdnme = self.codeName, mdl = self.model)
